619177c9b2842db13404b59c7adff071e27329a9
Julio Cézar <j.cezar4152@gmail.com>
2019-09-22 21:36:03 +0000

    Update raspd.py

--- blackmamba_bot/raspd.py
+++ blackmamba_bot/raspd.py
@@ -1,15 +1,29 @@
-from selenium import webdriver
-from bs4 import BeautifulSoup
-import time
+"""Para rodar o programa será necessario duas Bibliotecas(Selenium e BeautifulSoup)
+Para instalar é só rodar o seguinte comando no prompt da maquina:
 
-for contagem in range(0,3):
+pip install Selenium bs4
+
+"""
+from selenium import webdriver
+from bs4 import BeautifulSoup, SoupStrainer
+import time
+x=0
+while (x==0):
     ff=webdriver.Chrome()
-    ff.get('https://br.tradingview.com/symbols/BMFBOVESPA-BIDI4/')
+    ff.get('https://br.tradingview.com/symbols/BTCUSD/?exchange=COINBASE')
     html=ff.page_source
     soup=BeautifulSoup(html,"html.parser")
-    filtro1=soup.find_all(class_='tv-symbol-price-quote__value js-symbol-last')
-    filtro2=soup.find_all(class_='js-symbol-change tv-symbol-price-quote__change-value')
-    filtro3=soup.find_all(class_='js-symbol-change-pt tv-symbol-price-quote__change-value')
-    print (filtro1,filtro2,filtro3)
+    time.sleep(10)
+    ##-----------------------------------
+    filtro1 = soup.find_all(class_='tv-symbol-price-quote__value js-symbol-last')
+    filtro1 = filtro1[0].text[0:-1]
+    ##-----------------------------------
+    filtro2 = soup.find_all(class_='js-symbol-change tv-symbol-price-quote__change-value')
+    filtro2 = filtro2[0].text[0:]
+    ##-----------------------------------
+    filtro3 = soup.find_all(class_='js-symbol-change-pt tv-symbol-price-quote__change-value')
+    filtro3 = filtro3[0].text[1:-1]
+    print (filtro1,"\n",filtro2,"\n",filtro3)
+    print ("--------------------------------------------------------------------------------")
+    time.sleep(10)
     ff.quit()
-    time.sleep(20)
