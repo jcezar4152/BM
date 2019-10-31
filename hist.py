c33e299506b21731fa68ee18fd1f834cbcc3d37f
j.cezar4152 <j.cezar4152@gmail.com>
2019-10-30 21:46:29 -0300

    Correção do campo data

--- hist.py
+++ hist.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import pymysql
+import datetime
 ### as vareaveis abaixo sÃ£o contadores para pegar a posiÃ§Ã£o exata dos valores no html
 b=8
 c=9
@@ -26,69 +27,95 @@ connection = pymysql.connect(host='bvzfdagnfqepipz70gyw-mysql.services.clever-cl
                                  password='ZoKM7HXwAaZAgd9ugpTr')
 cursor = connection.cursor()
 while(valida==True):
-
-        data_acao=a[b].text
-
-        valor_fechado=a[c].text.replace(",",".")
-        valor_fechado=float(valor_fechado)
-
-        valor_minimo=a[d].text.replace(",",".")
-        valor_minimo=float(valor_minimo)
-
-        valor_maximo=a[e].text.replace(",",".")
-        valor_maximo=float(valor_maximo)
-
-        valor_variacao=a[f].text.replace(",",".")
-        valor_variacao=float(valor_variacao)
-
-        valor_porc=a[g].text.replace(",",".")
-        valor_porc=float(valor_porc)
-
-        volume=a[h].text.replace(".","")
-        volume=int(volume)
         try:
-                cursor.execute(
-                        "INSERT INTO bvzfdagnfqepipz70gyw.Historico(data_acao, valor_fechado, valor_minimo, valor_maximo, valor_variacao, variacao_porc, volume) VALUES('%s', '%f', '%f','%f','%f','%f','%f')"%(data_acao,valor_fechado,valor_minimo,valor_maximo,valor_variacao,valor_porc,volume))
-                connection.commit()
-                print(data_acao,"Inserido com sucesso")    
+                data_acao=a[b].text
+                data_acao=data_acao.replace('/','-')
+                data_acao=datetime.datetime.strptime(data_acao,'%d-%m-%Y')
+
+                valor_fechado=a[c].text.replace(",",".")
+                valor_fechado=float(valor_fechado)
+
+                valor_minimo=a[d].text.replace(",",".")
+                valor_minimo=float(valor_minimo)
+
+                valor_maximo=a[e].text.replace(",",".")
+                valor_maximo=float(valor_maximo)
+
+                valor_variacao=a[f].text.replace(",",".")
+                valor_variacao=float(valor_variacao)
+
+                valor_porc=a[g].text.replace(",",".")
+                valor_porc=float(valor_porc)
+
+                volume=a[h].text.replace(".","")
+                volume=int(volume)
+                try:
+                        cursor.execute(
+                                "INSERT INTO bvzfdagnfqepipz70gyw.Historico(data_acao, valor_fechado, valor_minimo, valor_maximo, valor_variacao, variacao_porc, volume) VALUES('%s', '%f', '%f','%f','%f','%f','%f')"%(data_acao,valor_fechado,valor_minimo,valor_maximo,valor_variacao,valor_porc,volume))
+                        connection.commit()
+                        print(data_acao,"Inserido com sucesso")
+                except:
+                        print(data_acao,"Já existe")
+                b=b+7
+                c=c+7
+                d=d+7
+                e=e+7
+                f=f+7
+                g=g+7
+                h=h+7
+                print(b)
+                ###Agora iremos alterar a pagina para pegar o restante dos dados, foi necessario zerar os contadores pois Ã© "uma nova pagina".
         except:
-                print(data_acao,"Já existe")      
-        b=b+7
-        c=c+7
-        d=d+7
-        e=e+7
-        f=f+7
-        g=g+7
-        h=h+7
-        ###Agora iremos alterar a pagina para pegar o restante dos dados, foi necessario zerar os contadores pois Ã© "uma nova pagina".
-        if(b==1401):
-                arq.close()
+                print("Primeira pagina OK")
                 nav=requests.get("http://200.147.99.191/acao/cotacoes-historicas.html?codigo=BIDI4.SA&beginDay="+diaI+"&beginMonth="+mesI+"&beginYear="+anoI+"&endDay="+diaF+"&endMonth="+mesF+"&endYear="+anoF+"&size=200&page=2&period=")
                 html=nav.text
                 soup=BeautifulSoup(html,'html.parser')
                 a=soup.findAll('td')        
-                b=8 # DATA da cotaÃ§Ã£o
-                c=9 # VALOR da cotaÃ§Ã£o (fechado)
-                d=10# VALOR minimo da integraÃ§Ã£o
-                e=11# VALOR maximo da cotaÃ§Ã£o
-                f=12# VALOR da variaÃ§Ã£o
-                g=13# VARIAÃÃO em porcentagem
-                h=14# VOLUME "transaÃ§Ãµes"
+                b=8 # DATA da cotacao
+                c=9 # VALOR da cotacao (fechado)
+                d=10# VALOR minimo da integracao
+                e=11# VALOR maximo da cotacao
+                f=12# VALOR da variacao
+                g=13# VARIACAO em porcentagem
+                h=14# VOLUME "transacoes"
                 while(valida==True):
                         try:
-                                cursor.execute(
-                                        "INSERT INTO bvzfdagnfqepipz70gyw.Historico(data_acao, valor_fechado, valor_minimo, valor_maximo, valor_variacao, variacao_porc, volume) VALUES('%s', '%f', '%f','%f','%f','%f','%f')"%(data_acao,valor_fechado,valor_minimo,valor_maximo,valor_variacao,valor_porc,volume))
-                                connection.commit()
-                                print(data_acao,"Inserido com sucesso")  
+                                data_acao = a[b].text
+                                data_acao = data_acao.replace('/', '-')
+                                data_acao = datetime.datetime.strptime(data_acao, '%d-%m-%Y')
+
+                                valor_fechado = a[c].text.replace(",", ".")
+                                valor_fechado = float(valor_fechado)
+
+                                valor_minimo = a[d].text.replace(",", ".")
+                                valor_minimo = float(valor_minimo)
+
+                                valor_maximo = a[e].text.replace(",", ".")
+                                valor_maximo = float(valor_maximo)
+
+                                valor_variacao = a[f].text.replace(",", ".")
+                                valor_variacao = float(valor_variacao)
+
+                                valor_porc = a[g].text.replace(",", ".")
+                                valor_porc = float(valor_porc)
+
+                                volume = a[h].text.replace(".", "")
+                                volume = int(volume)
+                                try:
+                                        cursor.execute(
+                                                "INSERT INTO bvzfdagnfqepipz70gyw.Historico(data_acao, valor_fechado, valor_minimo, valor_maximo, valor_variacao, variacao_porc, volume) VALUES('%s', '%f', '%f','%f','%f','%f','%f')"%(data_acao,valor_fechado,valor_minimo,valor_maximo,valor_variacao,valor_porc,volume))
+                                        connection.commit()
+                                        print(data_acao,"Inserido com sucesso")
+                                except:
+                                        print(data_acao,"Já existe")
+                                b=b+7
+                                c=c+7
+                                d=d+7
+                                e=e+7
+                                f=f+7
+                                g=g+7
+                                h=h+7
+                                print(b)
                         except:
-                                print(data_acao,"Já existe")      
-                        b=b+7
-                        c=c+7
-                        d=d+7
-                        e=e+7
-                        f=f+7
-                        g=g+7
-                        h=h+7
-                        if(b<=337):
                                 valida=False
 connection.close()
