3791a540d821a13671c9614c45efaa1c4551d4f6
j.cezar4152 <j.cezar4152@gmail.com>
2019-11-05 21:21:18 -0300

    att rasp twitter

--- BM_Twitter.py
+++ BM_Twitter.py
@@ -1,38 +1,26 @@
-from twitter_scraper import get_tweets
+import mysql.connector
 import datetime
-import pymysql
-#Consultar variação (gatilho):
-connection = pymysql.connect(host='bvzfdagnfqepipz70gyw-mysql.services.clever-cloud.com',
-                                 port=3306,
-                                 user='ufgpsjx1cswrmye3',
-                                 password='ZoKM7HXwAaZAgd9ugpTr')
-cursor = connection.cursor()
-cursor.execute("SELECT DATA_ACAO FROM bvzfdagnfqepipz70gyw.Historico WHERE data_acao='2019-10-31'")
-data_acao=cursor.fetchone()
-data=str(data_acao)
-data=data.replace(' ','')
-count=len(data)
-#historico=[]
-if(count==28):
-    data=data[15:25]
-    data=data.replace(',','-')
-    data=datetime.datetime.strptime(data,'%Y-%m-%d')
-    #historico.append(data)
-else:
-    if(count==27):
-        data = data[15:24]
-        data = data.replace(',', '-')
-        data = datetime.datetime.strptime(data, '%Y-%m-%d')
-        #historico.append(data)
-    else:
-        if(count==26):
-            data = data[15:23]
-            data = data.replace(',', '-')
-            data = datetime.datetime.strptime(data, '%Y-%m-%d')
-            #historico.append(data)
-#Recebendo a data
-for colet in get_tweets('bancointer'):
-    CD = colet['time'].date()
-    colet = colet['text']
-    if(CD==data.date()):
-        print(CD,colet)
+from twitter_scraper import get_tweets
+mydb = mysql.connector.connect(
+     host='bvzfdagnfqepipz70gyw-mysql.services.clever-cloud.com',
+     port=3306,
+     user='ufgpsjx1cswrmye3',
+     password='ZoKM7HXwAaZAgd9ugpTr')
+cursor=mydb.cursor()
+cursor.execute("SELECT DATA_ACAO FROM bvzfdagnfqepipz70gyw.Historico WHERE valor_variacao > 3 or valor_variacao<-3")
+result=cursor.fetchall()
+historico=[]
+x=0
+try:
+    while(x<=len(result)):
+        historico.append(str(result[x]).replace('(datetime.date(','').replace('),)','').replace(' ','').replace(',','-'))
+        x=x+1
+        print(x,"° data coletada")
+except:
+    x=0
+    for colet in get_tweets('bancointer'):
+        dcoleta = colet['time'].date()
+        colet = colet['text']
+        if(dcoleta==datetime.datetime.strptime(historico[x], '%Y-%m-%d').date):
+            print(colet)
+            x=x+1
