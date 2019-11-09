92cc5f8f47020192d4cb00af7d737d50a4e1eb55
j.cezar4152 <j.cezar4152@gmail.com>
2019-11-09 19:51:20 -0300

    Correção rasp Twitter

--- BM_Twitter.py
+++ BM_Twitter.py
@@ -14,13 +14,24 @@ x=0
 try:
     while(x<=len(result)):
         historico.append(str(result[x]).replace('(datetime.date(','').replace('),)','').replace(' ','').replace(',','-'))
+        print(datetime.datetime.strptime(historico[x],'%Y-%m-%d').date()," foi coletado")
         x=x+1
-        print(x,"° data coletada")
 except:
-    x=0
-    for colet in get_tweets('bancointer'):
-        dcoleta = colet['time'].date()
-        colet = colet['text']
-        if(dcoleta==datetime.datetime.strptime(historico[x], '%Y-%m-%d').date):
-            print(colet)
-            x=x+1
+    try:
+        print('----------------------------------------------------------------')
+        x=len(result)-1
+        for colet in get_tweets('bancointer'):
+            dcoleta = colet['time'].date()
+            colet = colet['text']
+            data=datetime.datetime.strptime(historico[x],'%Y-%m-%d').date()
+            #print(data,' ',dcoleta)
+            #print(x)
+            if (data > dcoleta):
+                x = x - 1
+            else:
+                if(dcoleta==data):
+                    print(dcoleta,colet)
+                    x=x-1
+    except:
+        if(x<=0):
+            print('Concluido')
