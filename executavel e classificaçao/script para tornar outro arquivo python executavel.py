804051155fafa4b22eb11bdf8faf4b6777eeb25f
Arthur Cardoso Rinaldi da Silva <arthur_rinaldi00@hotmail.com>
2019-11-13 23:20:37 +0000

    Update executavel e classificaçao/script para tornar outro arquivo python executavel.py

---
+++ executavel e classificaçao/script para tornar outro arquivo python executavel.py
@@ -0,0 +1,26 @@
+import sys
+from cx_Freeze import setup, Executable
+import Classificacao
+
+base = None
+if sys.platform == "win64":
+    base = "Win64GUI"
+
+Executables = [
+    Executable("Classificacao.py", base=base)
+]
+
+buildOptions = dict(
+    packages = [],
+    includes = ["Classificacao"],
+    include_files = [],
+    excludes = []
+)
+
+setup(
+    name = "Classificacao",
+    version="1.0",
+    description = "Espero que funcione",
+    options = dict(build_exe = buildOptions),
+    Executables = Executables
+)
