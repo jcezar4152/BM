264369474a2a0d048a857c7aefb651f85043d447
Sabrina Rafaela Calado Mariano <sabrinarcmariano@gmail.com>
2019-11-18 03:04:46 -0200

    Base nova do front

--- BM front/bm_site/django_config/urls.py
+++ BM front/bm_site/django_config/urls.py
@@ -14,8 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf.urls import url
+from django.urls import path
 
 urlpatterns = [
-    url(r'link_name_to_access^$', TemplateView.as_view(template_name="index.html")),
+    path('admin/', admin.site.urls),
 ]
